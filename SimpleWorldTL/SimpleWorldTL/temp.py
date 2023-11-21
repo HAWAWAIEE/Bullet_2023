b9cc9e532292b0b3a5a47f148c68623c4e8c7d07
singuan <108323960+singuan@users.noreply.github.com>
2023-11-21 19:44:53 +0900

    1

--- SimpleWorldTL/SimpleWorldTL/temp.py
+++ SimpleWorldTL/SimpleWorldTL/temp.py
@@ -5,14 +5,15 @@ import gymnasium as gym
 import BigWorldTest20
 from stable_baselines3.common.env_util import make_vec_env
 from utils import(SB3ToTorchNN, nnKeyChanger)
-policy_file_path = r"C:\Users\shann\Desktop\PROGRAMMING\Python\Past_Results\BigEnv_Normal_16workers_4map_10000000timesteps_Results\nn\policy.pth"
+import numpy as np
+policy_file_path = r"C:\Users\shann\Desktop\PROGRAMMING\Python\Past_Results\Last_SimleEnvTL20\nn\policy.pth"
 variables_file_path = r"C:\Users\shann\Desktop\PROGRAMMING\Python\Past_Results\SimpleEnvTL28_16workers_4maps_10000000timesteps_Results\NN\pytorch_variables.pth"
 model_file_path = r"C:\Users\shann\Desktop\PROGRAMMING\Python\Past_Results\SimpleEnvTL28_16workers_4maps_10000000timesteps_Results\NN.zip"
 input_dim = 20
 output_dim = 2
 
 model = SB3ToTorchNN(input_dim, output_dim)
-model_state_dict = nnKeyChanger(torch.load(r"C:\Users\shann\Desktop\PROGRAMMiNG\Python\Past_Results\BigEnv_Normal_16workers_4map_10000000timesteps_Results\nn\policy.pth", map_location=torch.device('cpu')))
+model_state_dict = nnKeyChanger(torch.load(policy_file_path, map_location=torch.device('cpu')))
 model.load_state_dict(model_state_dict)
 env = BigWorldTest20.bigMapEnv(1)
 
@@ -20,6 +21,7 @@ while(True):
     observation = env.reset()
     done = False
     while not done:
+        actual_observation = torch.from_numpy(np.array(observation)).float()
         action = model.actorForward(torch.from_numpy(observation).float())
-
+        print(model.criticForward)
         observation, reward, done, info = env.step(action)
--- SimpleWorldTL/SimpleWorldTL/test.py
+++ SimpleWorldTL/SimpleWorldTL/test.py
@@ -6,6 +6,7 @@ import gymnasium as gym
 import numpy as np
 import BigWorldTest20
 from stable_baselines3.common.env_util import make_vec_env
+from utils import(SB3ToTorchNN, nnKeyChanger)
 
 policy_file_path = r"C:\Users\shann\Desktop\PROGRAMMING\Python\Past_Results\BigEnv_Normal_16workers_4map_10000000timesteps_Results\nn\policy.pth"
 variables_file_path = r"C:\Users\shann\Desktop\PROGRAMMING\Python\Past_Results\SimpleEnvTL28_16workers_4maps_10000000timesteps_Results\NN\pytorch_variables.pth"
@@ -13,57 +14,57 @@ model_file_path = r"C:\Users\shann\Desktop\PROGRAMMiNG\Python\Past_Results\Simpl
 pth_file_path = r"C:\Users\shann\Desktop\PROGRAMMiNG\Python\Results\NN\torchver.pth"
 input_dim = 20
 output_dim = 2
-
-def nnKeyChanger(model_state_dict):
-    new_state_dict = {}
+# Old Version But Stable
+# def nnKeyChanger(model_state_dict):
+#     new_state_dict = {}
     
-    for key in model_state_dict.keys():
-        if 'log_std' in key:
-            continue
-        new_key = key
-        if 'mlp_extractor.policy_net' in key:
-            new_key = key.replace('mlp_extractor.policy_net', 'actor')
-        elif 'mlp_extractor.value_net' in key:
-            new_key = key.replace('mlp_extractor.value_net', 'critic')
-        elif 'action_net' in key:
-            new_key = key.replace('action_net', 'actor.4')
-        elif 'value_net' in key:
-            new_key = key.replace('value_net', 'critic.4')
-        new_state_dict[new_key] = model_state_dict[key]
-    return new_state_dict
+#     for key in model_state_dict.keys():
+#         if 'log_std' in key:
+#             continue
+#         new_key = key
+#         if 'mlp_extractor.policy_net' in key:
+#             new_key = key.replace('mlp_extractor.policy_net', 'actor')
+#         elif 'mlp_extractor.value_net' in key:
+#             new_key = key.replace('mlp_extractor.value_net', 'critic')
+#         elif 'action_net' in key:
+#             new_key = key.replace('action_net', 'actor.4')
+#         elif 'value_net' in key:
+#             new_key = key.replace('value_net', 'critic.4')
+#         new_state_dict[new_key] = model_state_dict[key]
+#     return new_state_dict
 
-class SB3ToTorchNN(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super().__init__()
-        self.actor = nn.Sequential(
-            nn.Linear(input_dim, 64),
-            nn.Tanh(),
-            nn.Linear(64, 64),
-            nn.Tanh(),
-            nn.Linear(64, output_dim)
-        )
+# class SB3ToTorchNN(nn.Module):
+#     def __init__(self, input_dim, output_dim):
+#         super().__init__()
+#         self.actor = nn.Sequential(
+#             nn.Linear(input_dim, 64),
+#             nn.Tanh(),
+#             nn.Linear(64, 64),
+#             nn.Tanh(),
+#             nn.Linear(64, output_dim)
+#         )
         
-        self.critic = nn.Sequential(
-            nn.Linear(input_dim, 64),
-            nn.Tanh(),
-            nn.Linear(64, 64),
-            nn.Tanh(),
-            nn.Linear(64, 1)
-        )
+#         self.critic = nn.Sequential(
+#             nn.Linear(input_dim, 64),
+#             nn.Tanh(),
+#             nn.Linear(64, 64),
+#             nn.Tanh(),
+#             nn.Linear(64, 1)
+#         )
 
-    def actorForward(self, x):
-        mean = self.actor(x)
-        log_std = torch.zeros_like(mean)
+#     def actorForward(self, x):
+#         mean = self.actor(x)
+#         log_std = torch.zeros_like(mean)
 
-        std = torch.exp(log_std)
+#         std = torch.exp(log_std)
 
-        dist = torch.distributions.Normal(mean, std)
-        actions = dist.sample()
-        return actions
+#         dist = torch.distributions.Normal(mean, std)
+#         actions = dist.sample()
+#         return actions
         
-    def valueForward(self,x):
-        state_value = self.critic(x) 
-        return state_value
+#     def valueForward(self,x):
+#         state_value = self.critic(x) 
+#         return state_value
 
 model = SB3ToTorchNN(input_dim, output_dim)
 model_state_dict = nnKeyChanger(torch.load(policy_file_path, map_location=torch.device('cpu')))
@@ -78,6 +79,7 @@ while(True):
     while not done:
         actual_observation = torch.from_numpy(np.array(observation)).float()
         action = model.actorForward(actual_observation)
+        print(model.criticForward(actual_observation))
         observation, reward, done, info, truncated = env.step(action)
         time.sleep(.480)
     print(reward)
